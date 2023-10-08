96fa18a964070a5af6c8c343a66e2dd1f22bac4f
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2023-10-08 20:33:12 +0900

    Feat : BoxReducer 생성

--- src/utils/grid.tsx
+++ src/utils/grid.tsx
@@ -1,39 +1,47 @@
 import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { updateGridData } from "@reducer/gridSlice";
 import Modal from "@utils/modal";
+import { RootState } from "@store/index";
 
 const GridComponent = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [urlData, setUrlData] = useState({ title: "", artist: "", url: "" });
+  const [currentIndex, setCurrentIndex] = useState<number | null>(null);
+  const urlData = useSelector((state: RootState) => state.grid);
+  const dispatch = useDispatch();
 
-  const openModal = () => {
+  const openModal = (index: number) => {
     setIsModalOpen(true);
+    setCurrentIndex(index);
   };
 
   const updateUrlData = (title: string, artist: string, url: string) => {
-    setUrlData({ title, artist, url });
+    if (currentIndex !== null && currentIndex !== undefined) {
+      dispatch(updateGridData({ index: currentIndex, title, artist, url }));
+    }
     setIsModalOpen(false);
   };
 
-  const gridData = [0, 1, 2, 3];
-
   return (
     <div className="flex justify-center items-center gap-2 my-20">
-      <div className="grid grid-cols-2 grid-rows-2 gap-4 text-white">
-        {gridData.map((index) => (
+      <div className="grid grid-cols-2 grid-rows-2 gap-4 mt-10 text-white">
+        {urlData.map((data, index) => (
           <div
             key={index}
             className="w-[160px] h-[160px] rounded-2xl border border-white"
           >
-            <button onClick={openModal}>새로운 플레이리스트</button>
+            <button onClick={() => openModal(index)}>
+              새로운 플레이리스트
+            </button>
           </div>
         ))}
       </div>
 
-      {isModalOpen && (
+      {isModalOpen && currentIndex !== null && (
         <Modal
           isOpen={isModalOpen}
           onClose={() => setIsModalOpen(false)}
-          urlData={urlData}
+          urlData={urlData[currentIndex]}
           updateUrlData={updateUrlData}
         />
       )}
