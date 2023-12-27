d719120f8ad5085f5be2db50f8bca4b1afa5b734
ys0222 <ggdbstjd0222@gmail.com>
2023-12-27 14:48:06 +0900

    Fix : Web 화면시 customMargin 버그 픽스

--- src/components/Admin/AddPLayList.tsx
+++ src/components/Admin/AddPLayList.tsx
@@ -11,23 +11,23 @@ export const AddPalyList = () => {
   const [customMargin,setCustomMargin] = useState<number>(0);
 
   useEffect(()=>{
-    if(windowSize.width <= 390){
       setCustomMargin(Math.floor(((windowSize.width)/2-150)/2));
-    }
     
     console.log(customMargin)
   },[windowSize.width,customMargin])
   
   return (
     <div style={{margin: `${customMargin}px`}} className="inline ">
-      <button
-        className="w-[150px] h-[150px] rounded-[13px] border-2 border-zinc-300 font-light text-zinc-300 text-4xl mt-[42px]"
-      >
-        <Link to='13'>
+      <Link to='13'>
+
+        <button
+          className="w-[150px] h-[150px] rounded-[13px] border-2 border-zinc-300 font-light text-zinc-300 text-4xl mt-[42px]"
+        >
           +
-          <div className="text-center text-zinc-300 text-[13px] font-medium font-['Noto Sans'] leading-[18px]">New PlayList</div>
-        </Link>
-      </button>
+        <div className="text-center text-zinc-300 text-[13px] font-medium font-['Noto Sans'] leading-[18px]">New PlayList</div>
+      
+        </button>
+      </Link>
     </div>
   )
 }
--- src/pages/Admin/AdminPage.tsx
+++ src/pages/Admin/AdminPage.tsx
@@ -183,6 +183,10 @@ const calculateOptionsModalPosition = (e: { target: any; }) => {
 
 
       <AddPalyList />
+      <AddPalyList />
+
+      <AddPalyList />
+      <AddPalyList />
     </div>
     </div>
     </Layout>
