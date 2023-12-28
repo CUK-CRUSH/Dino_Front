2d1eb817ccb30c245c63c35d3d2d70590a79c490
ys0222 <ggdbstjd0222@gmail.com>
2023-12-28 17:02:42 +0900

    Fix : 빌드 에러 픽스2

--- src/pages/Admin/AdminPage.tsx
+++ src/pages/Admin/AdminPage.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux"; // Import the useDispatch hook
-import { selectImage } from "@reducer/imageSlice";
 import { RootState } from "@store/index";
 import AdminEditModal from "./AdminEditModal";
 import Layout from "@components/Layout/layout";
@@ -11,7 +10,6 @@ interface AdminPageProps { }
 
 const AdminPage: React.FC<AdminPageProps> = () => {
   const selectedImage = useSelector((state: RootState) => state.image.selectedImage);
-  const dispatch = useDispatch(); // Initialize dispatch
 
   const [isEditModalOpen, setIsEditModalOpen] = useState(false); // Local state to manage the modal
 
