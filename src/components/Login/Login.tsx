5aec40673e8bcdf32c80eacf8e73c179b8e46b13
ys0222 <ggdbstjd0222@gmail.com>
2024-01-18 17:18:36 +0900

    Fix : 빌드 에러 수정

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,6 +1,5 @@
 // @ts-ignore
 
-import { Link } from "react-router-dom";
 import mylist2 from "@assets/Mylist2.png";
 import googlelogo from "@assets/Google logo.png";
 import facebook from "@assets/facebook-3 logo.png";
