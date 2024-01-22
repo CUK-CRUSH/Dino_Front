c9fca51b5041f66cc9f405ac134d028e2ca6850f
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2024-01-22 16:24:23 +0900

    Fix -CI/CD

--- src/components/AdminEdit/Button/EditButton.tsx
+++ src/components/AdminEdit/Button/EditButton.tsx
@@ -1,18 +1,19 @@
-import { MouseEventHandler } from "react";
-import { EditButtonDTO, UpdateMemberParams } from "types/AdminEdit";
+import { EditButtonDTO } from "types/AdminEdit";
 
-
-const EditButton = ({save, cancel, updateMemberData} : EditButtonDTO) => {
+const EditButton = ({ save, cancel, updateMemberData }: EditButtonDTO) => {
   return (
     <>
       <button onClick={cancel} className="text-red-600 ml-[10px]">
         Cancel
       </button>
-      <button onClick={() => save(updateMemberData)} className="text-gray-800 mr-[10px]">
+      <button
+        onClick={() => save(updateMemberData)}
+        className="text-gray-800 mr-[10px]"
+      >
         Save
       </button>
     </>
-  )
-}
+  );
+};
 
 export default EditButton;
