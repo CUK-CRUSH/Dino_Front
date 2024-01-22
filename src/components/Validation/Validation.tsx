6e4ac6612f75ac1737e65f7518e1376b4baacef5
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2024-01-22 10:59:13 +0900

    Delete - Test

--- src/components/Validation/Validation.tsx
+++ src/components/Validation/Validation.tsx
@@ -2,31 +2,30 @@ import { debounce } from "lodash";
 import Fanfare from "../../assets/Validation/Fanfare.svg";
 import Check from "../../assets/Validation/Check.svg";
 import not from "../../assets/Validation/not.svg";
-
 import { BsFillExclamationCircleFill } from "react-icons/bs";
 import { useState } from "react";
 import { useCookies } from "react-cookie";
-import { getNicknameAvailable, putUsername } from "@api/member-controller/memberController";
+import {
+  getNicknameAvailable,
+  putUsername,
+} from "@api/member-controller/memberController";
 import { checkBadWord } from "@utils/checkBadWord/checkBadWord";
 import { useNavigate } from "react-router-dom";
 import useDecodedJWT from "@hooks/useDecodedJWT";
-  
-  // 닉네임 체크
-  export const checkNickname = (nickname : string) => {
-    // 숫자영어 _ . 허용
-    const nicknameRegex = /^[a-zA-Z0-9._]{3,30}$/;
-    console.log(nickname)
-    if(nicknameRegex.test(nickname)){
-      if(!checkBadWord(nickname)){
-        return true
-      }
+
+// 닉네임 체크
+export const checkNickname = (nickname: string) => {
+  // 숫자영어 _ . 허용
+  const nicknameRegex = /^[a-zA-Z0-9._]{3,30}$/;
+  console.log(nickname);
+  if (nicknameRegex.test(nickname)) {
+    if (!checkBadWord(nickname)) {
+      return true;
     }
-    else if(!nicknameRegex.test(nickname)){
-      
-        return false
-      
-    }
-  };
+  } else if (!nicknameRegex.test(nickname)) {
+    return false;
+  }
+};
 // import { checkNickname } from "@utils/checkNickname/checkNickname";
 
 const ValidationProps = () => {
@@ -36,55 +35,57 @@ const ValidationProps = () => {
   const [nicknameValidation, setNicknameValidation] = useState<boolean>(false);
 
   // 유저네임
-  const [username, setUsername] = useState<string>('');
+  const [username, setUsername] = useState<string>("");
 
   // 쿠키
-  const [cookies,] = useCookies(["accessToken"]);
-  // 액세스 토큰 
+  const [cookies] = useCookies(["accessToken"]);
+  // 액세스 토큰
   const token = cookies.accessToken;
 
   const decodedToken = useDecodedJWT(token);
 
   const onChange = debounce(async (e) => {
     setUsername(e.target.value);
-    
-    
-    if(e.target.value){
+
+    if (e.target.value) {
       // Backend 닉네임 체크
-      const checkNicknameBack = await getNicknameAvailable(e.target.value,token);
-      console.log(checkNicknameBack)
+      const checkNicknameBack = await getNicknameAvailable(
+        e.target.value,
+        token
+      );
+      console.log(checkNicknameBack);
 
-      if(checkNickname(e.target.value) && checkNicknameBack.status === 200 ){
+      if (checkNickname(e.target.value) && checkNicknameBack.status === 200) {
         setNicknameValidation(true);
-      }
-
-      else if(!checkNickname(e.target.value) && checkNicknameBack.status !== 200) {
+      } else if (
+        !checkNickname(e.target.value) &&
+        checkNicknameBack.status !== 200
+      ) {
         setNicknameValidation(false);
-      }
-      else {
+      } else {
         setNicknameValidation(false);
       }
     } else {
-        setNicknameValidation(false);
+      setNicknameValidation(false);
     }
   }, 500);
 
   console.log(cookies);
 
-  const handleUsername = async (username : string,cookies :string) => {
-    console.log(username,cookies)
+  const handleUsername = async (username: string, cookies: string) => {
+    console.log(username, cookies);
 
-    const putUsernameState = await putUsername(username,cookies);
-    console.log(putUsernameState)
-    if(putUsernameState.status === 200) {
+    const putUsernameState = await putUsername(username, cookies);
+    console.log(putUsernameState);
+    if (putUsernameState.status === 200) {
       navigate(`/${decodedToken.sub}/admin`);
     }
-  }
+  };
 
   return (
     <div className="w-full h-full relative bg-white flex flex-col align-middle items-center">
       <div className="text-center text-black text-xl font-semibold font-['Noto Sans'] my-10">
-        <img className="mx-auto mt-16 mb-10" src={Fanfare} alt='Fanfare' />
+        <img className="mx-auto mt-16 mb-10" src={Fanfare} alt="Fanfare" />
         환영합니다 !
         <br />
         사용하실 닉네임을 작성해주세요 !
@@ -95,19 +96,24 @@ const ValidationProps = () => {
         </span>
       </div>
       <div className="w-11/12 mb-4 flex flex-col items-center relative focus:border-slate-300">
-      <input
+        <input
           type="text"
           onChange={(e) => onChange(e)}
           placeholder={"닉네임"}
           className=" w-11/12 p-2 pr-12 border 1px bg-white rounded-3xl text-center border-slate-200	focus:outline-none "
         />
         <div className="absolute right-10 top-3">
-          {nicknameValidation ? <img src={Check} alt="Edit" className="w-4 h-4 cursor-pointer" /> : <img src={not} alt="Edit" className="w-4 h-4 cursor-pointer" /> }
+          {nicknameValidation ? (
+            <img src={Check} alt="Edit" className="w-4 h-4 cursor-pointer" />
+          ) : (
+            <img src={not} alt="Edit" className="w-4 h-4 cursor-pointer" />
+          )}
         </div>
       </div>
-      <div 
+      <div
         className="fixed w-full h-[70px] mx-auto bottom-0 pt-6 text-center text-white bg-black"
-        onClick={() => handleUsername(username, cookies.accessToken)}>
+        onClick={() => handleUsername(username, cookies.accessToken)}
+      >
         계속하기
       </div>
     </div>
