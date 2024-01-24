7ef599ec84759090853daab94a001ba5092ebcf9
ys0222 <ggdbstjd0222@gmail.com>
2024-01-24 14:26:32 +0900

    Fix : api 경로 수정

--- src/api/member-controller/memberController.tsx
+++ src/api/member-controller/memberController.tsx
@@ -5,7 +5,7 @@ import { UpdateMemberParams } from "types/AdminEdit";
 export const putUsername = async (username: string, cookies?: string) => {
   try {
     const response = await axiosInstance.put(
-      `/api/v1/member/nickname/${username}`,
+      `/api/v1/member/me/${username}`,
       null,
       {
         headers: {
