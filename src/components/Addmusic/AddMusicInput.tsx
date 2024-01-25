e2e69a9e38d0a43de34cfbf5b12c87499f810cf3
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2024-01-25 16:13:01 +0900

    Feat - 자동완성 absolute 고정 및 hidden

--- src/components/Addmusic/AddMusicInput.tsx
+++ src/components/Addmusic/AddMusicInput.tsx
@@ -1,5 +1,5 @@
 import { MusicInputDTO } from "types/Addmusic/AddMusic";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { IoInformationCircleOutline } from "react-icons/io5";
 
 export const AddMusicInput: React.FC<MusicInputDTO> = ({
@@ -12,15 +12,40 @@ export const AddMusicInput: React.FC<MusicInputDTO> = ({
   infoText = "",
   infoToggleHandler = () => {},
   suggestions = [],
-  onSuggestionClick = () => {},
+  onSuggestionClick,
 }) => {
+  const [isSuggestionsVisible, setIsSuggestionsVisible] = useState(false);
+  const wrapperRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (
+        wrapperRef.current &&
+        !wrapperRef.current.contains(event.target as Node)
+      ) {
+        setIsSuggestionsVisible(false);
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onChange(e);
+    setIsSuggestionsVisible(true);
+  };
+
+  const handleSuggestionClick = (suggestion: string) => {
+    if (onSuggestionClick) {
+      onSuggestionClick(suggestion);
+    }
+    setIsSuggestionsVisible(false);
+  };
+
   return (
-    <div className="relative">
-      {suggestions.map((suggestion, index) => (
-        <div key={index} onClick={() => onSuggestionClick(suggestion)}>
-          {suggestion}
-        </div>
-      ))}
+    <div className="relative" ref={wrapperRef}>
       <div className="flex flex-row ">
         <h3 className="text-[17px] leading-[18px] mb-3 mr-1">{label}</h3>
         <div className="mt-[3px]">
@@ -39,8 +64,17 @@ export const AddMusicInput: React.FC<MusicInputDTO> = ({
         className="w-full px-2 py-3 border bg-black border-white rounded-[3px]"
         value={value}
         required={required}
-        onChange={onChange}
+        onChange={handleChange}
       />
+      {isSuggestionsVisible && (
+        <div className="absolute top-full w-full mt-2 border-white border-b-2  border-x-2 bg-red-400 z-10">
+          {suggestions.map((suggestion, index) => (
+            <div key={index} onClick={() => handleSuggestionClick(suggestion)}>
+              {suggestion}
+            </div>
+          ))}
+        </div>
+      )}
       {infoButton && infoText && (
         <div className="absolute -bottom-16 w-full px-2 py-3 bg-[#3B3B3B] rounded-xl">
           <p>{infoText}</p>
