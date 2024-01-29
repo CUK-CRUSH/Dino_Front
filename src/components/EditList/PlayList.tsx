6e94efb57facd568e2dbf43a4f62c8d70f80dae2
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2024-01-29 17:50:59 +0900

    Fix - 불필요한 handleCompressImage 제거

--- src/components/EditList/PlayList.tsx
+++ src/components/EditList/PlayList.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { UsePlayListEditor } from "@hooks/UsePlayListEditor";
 import { RootState } from "@store/index";
@@ -6,7 +6,6 @@ import { EditPlsyListDTO } from "types/EditplayList";
 import { EditPlaylistControls } from "@components/EditList/Button/EditPlaylistControl";
 import { MusicDataRow } from "@components/EditList/MusicList/MusicDataRow";
 import useImageCompress from "@hooks/useImageCompress";
-import { dataURItoFile } from "@utils/ImageCrop/common";
 import { PlusButton } from "@components/EditList/Button/PlusButton";
 import ShowImage from "@components/EditList/EditImage/ShowImage";
 import { MainEditButton } from "@components/EditList/Button/MainEditButton";
@@ -25,7 +24,7 @@ const PlayList: React.FC<EditPlsyListDTO> = () => {
   const musicData = useSelector((state: RootState) => state.musicAdd);
   const [uploadImage, setUploadImage] = useState<string | null>(null);
 
-  const { isLoading: isCompressLoading, compressImage } = useImageCompress();
+  const { isLoading: isCompressLoading } = useImageCompress();
   const [playlists, setPlaylists] = useState<any[]>([]);
   const [username, setUsername] = useState<string | null>(null);
   const [playlistName, setPlaylistName] = useState("");
@@ -34,13 +33,7 @@ const PlayList: React.FC<EditPlsyListDTO> = () => {
   // const [page, setPage] = useState<number>(0);
 
   const handleUploadImage = (image: string) => setUploadImage(image);
-  const handleCompressImage = useCallback(async () => {
-    if (!uploadImage) return;
 
-    const result = dataURItoFile(uploadImage);
-
-    if (!result) return;
-  }, [uploadImage, compressImage]);
   // 쿠키에서 유저 id 가져오기
   const [cookies] = useCookies(["accessToken"]);
   const token = cookies.accessToken;
@@ -63,9 +56,6 @@ const PlayList: React.FC<EditPlsyListDTO> = () => {
   );
 
   useEffect(() => {
-    if (uploadImage) {
-      handleCompressImage();
-    }
     console.log(uploadImage);
     const fetchPlaylist = async (id: number) => {
       const member = await getMember(id);
@@ -79,7 +69,7 @@ const PlayList: React.FC<EditPlsyListDTO> = () => {
     if (id !== undefined) {
       fetchPlaylist(id);
     }
-  }, [musicList, id, uploadImage, handleCompressImage, playlistId]);
+  }, [musicList, id, uploadImage, playlistId]);
   // 일단 의존성때문에 넣을건데 musicList빼고 나중에 다 지워도 될ㄷ스.
   // /[musicList, id, uploadImage, handleCompressImage, playlistId]
 
