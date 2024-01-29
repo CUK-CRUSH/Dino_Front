b4c1f370d4311f224467f46557e3b9a34e40707f
ys0222 <ggdbstjd0222@gmail.com>
2024-01-29 16:23:16 +0900

    Rename : 폴더 이름 변경

--- src/components/Admin/UserProfileInfo.tsx
+++ src/components/Admin/UserProfileInfo.tsx
@@ -1,4 +1,4 @@
-import Skeleton from "@components/Skeleton.tsx/Skeleton";
+import Skeleton from "@components/Skeleton/Skeleton";
 import { RootState } from "@store/index";
 import React from "react";
 import { useEffect, useState } from "react";
