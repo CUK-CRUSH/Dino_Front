ac563174e217d8cc2aacd5702550d527904bcf76
ys0222 <ggdbstjd0222@gmail.com>
2024-02-01 10:31:04 +0900

    Fix : 라우팅 변경

--- src/components/SetProfile/Skip.tsx
+++ src/components/SetProfile/Skip.tsx
@@ -38,14 +38,14 @@ const Skip = ({ step,username }: SkipDTO) => {
       const code = await updateMember(data);
       if(code.status === 200) {
         console.log(code)
-        navigate(`/${code.data.username}/admin`)
+        navigate(`/${code.data.username}`)
       }
     }
   
   };
   
   useEffect(() => {
-    if (step >= 4) { navigate(`/${username}/admin`); }
+    if (step >= 4) { navigate(`/${username}`); }
   }, [step,navigate,username])
 
   const handleClick = () => {
