80f3c5c15ee646889ba568e30b43803db5a5ef92
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2024-02-02 19:02:41 +0900

    Design - Footer

--- src/components/Layout/footer.tsx
+++ src/components/Layout/footer.tsx
@@ -1,6 +1,10 @@
-const Footer = () => {
+interface FooterProps {
+  bgColor: string;
+}
+
+const Footer = ({ bgColor }: FooterProps) => {
   return (
-    <footer className="text-[#A1A1AA] text-xs text-center bg-black p-4">
+    <footer className={`text-[#A1A1AA] text-xs text-center bg-${bgColor} p-4`}>
       <p className="mb-2">ASK</p>
       <p className="mb-2">Instagram : @mylist.official</p>
       <p className="mb-2">Email : mylist.company@gmail.com</p>
