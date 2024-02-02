b2d51caedbbcd3d2190d0fbeba09690fd613f68b
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2024-02-02 17:21:59 +0900

    Feat - Footer생성

---
+++ src/components/Layout/footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer className="text-[#A1A1AA] text-xs text-center bg-black p-4">
+      <p className="mb-2">ASK</p>
+      <p className="mb-2">Instagram : @mylist.official</p>
+      <p className="mb-2">Email : mylist.company@gmail.com</p>
+      <p>MyList Copyright ⓒ TEAM DINO. All Rights Reserved</p>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/Layout/layout.tsx
+++ src/components/Layout/layout.tsx
@@ -7,8 +7,7 @@ import {
 } from "@reducer/musicadd";
 import { usePreviousLocation } from "@utils/RouteRedux/isRouting";
 import React, { useEffect } from "react";
-import { batch } from "react-redux";
-import { useDispatch } from "react-redux";
+import { batch, useDispatch } from "react-redux";
 import { useLocation } from "react-router-dom";
 import { LayoutDTO } from "types/layout";
 
@@ -34,7 +33,7 @@ const Layout: React.FC<LayoutDTO> = ({ children }) => {
     }
   }, [location.pathname, prevLocation.pathname, dispatch]);
   return (
-    <div className="overflow-hidden bg-[#111111]">
+    <div className="overflow-hidden  scrollbar-hide bg-[#111111]">
       <div className="h-full w-full max-h-full flex justify-center">
         <main className="max-w-[390px] smartPhone:max-w-[430px] w-full h-screen overflow-y-auto overflow-x-hidden relative font-PretendardRegular">
           {children}
