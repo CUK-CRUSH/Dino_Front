d56d0a727f14fbfcf10ab49f1894c6081655eb86
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2024-02-02 15:13:18 +0900

    Feat - 플리삭제에 모달넣기

--- src/hooks/UsePlayListEditor.ts
+++ src/hooks/UsePlayListEditor.ts
@@ -1,4 +1,4 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import {
   updateArtist,
   updateTitle,
@@ -14,10 +14,11 @@ import {
 import { postMusicList } from "@api/music-controller/musicControl";
 import { useNavigate } from "react-router-dom";
 import { setToast } from "@reducer/Toast/toast";
-import { useSelector } from "react-redux";
 import { RootState } from "@store/index";
 import useImageCompress from "./useImageCompress";
 import { resetSelectedFile } from "@reducer/editPlayList/Image/isImageCompress";
+import Swal from "sweetalert2";
+import "@styles/EditList/playList.css";
 
 interface UsePlayListEditorProps {
   playlists: any[];
@@ -46,6 +47,18 @@ export const UsePlayListEditor = ({
   const isLoading = useSelector(
     (state: RootState) => state.selectedFile.isLoading
   );
+
+  const swalButton = Swal.mixin({
+    customClass: {
+      popup: "popup", // 전체
+      confirmButton: "confirmButton", // 취소
+      cancelButton: "cancelButton", // 삭제
+      title: "title", // 타이틀
+      htmlContainer: "htmlContainer", // 내용
+    },
+    buttonsStyling: false,
+  });
+
   const readImageFile = (file: File) => {
     return new Promise<string>((resolve, reject) => {
       const reader = new FileReader();
@@ -69,10 +82,10 @@ export const UsePlayListEditor = ({
     const playlist = playlists.find(
       (playlist: any) => playlist?.id === Number(playlistId)
     );
-  
+
     if (playlist) {
       const id = playlist.id;
-  
+
       if (selectedFile) {
         const compressedFile = await compressImage(selectedFile);
         if (compressedFile) {
@@ -88,12 +101,12 @@ export const UsePlayListEditor = ({
           }
         }
       }
-  
+
       // 비동기 함수들이 완료될 때까지 기다립니다.
       if (playlistName) {
         await putPlayList(id, playlistName, null, token);
       }
-  
+
       // 이미지 저장이 완료된 후에 음악 추가를 진행하도록 변경
       if (musicData && musicData.title && musicData.artist && musicData.url) {
         await postMusicList(
@@ -109,7 +122,7 @@ export const UsePlayListEditor = ({
         dispatch(updateImage(null));
       }
     }
-  
+
     dispatch(setToast("editPlayList"));
     dispatch(setIsEditing(false));
     dispatch(resetIsSaved());
@@ -117,7 +130,6 @@ export const UsePlayListEditor = ({
       dispatch(updateImage(compressedImage));
     }
   };
-  
 
   const handleCancelClick = () => {
     dispatch(updateTitle(""));
@@ -130,8 +142,33 @@ export const UsePlayListEditor = ({
 
   //플리삭제
   const handleDeleteClick = async () => {
-    await deletePlayList(playlistId ?? "", token);
-    navigate(`/${username}`);
+    swalButton
+      .fire({
+        title: "노래를 삭제하시겠습니까?",
+        html: "한번 삭제된 노래는 복구할 수 없습니다!",
+        showCancelButton: true,
+        confirmButtonColor: "blue",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "취소",
+        cancelButtonText: "삭제",
+      })
+      .then(async (result) => {
+        if (result.isConfirmed) {
+          // '취소' 버튼을 눌렀을 때 실행할 코드를 여기에 작성합니다.
+        } else if (result.dismiss === Swal.DismissReason.cancel) {
+          // '삭제' 버튼을 눌렀을 때 실행할 코드를 여기에 작성합니다.
+          try {
+            await deletePlayList(playlistId ?? "", token);
+            navigate(`/${username}`);
+          } catch (error) {
+            console.log(error);
+            swalButton.fire({
+              title: "노래 삭제에 실패했습니다.",
+              width: "250px",
+            });
+          }
+        }
+      });
   };
 
   return {
