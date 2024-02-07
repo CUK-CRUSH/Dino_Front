37e580c32390f9c548ac3759ded1d3d4658a84a9
ys0222 <ggdbstjd0222@gmail.com>
2024-02-07 16:58:24 +0900

    Design : ... 옵션창 디자인 변경

--- src/components/Admin/Button/OpenOption.tsx
+++ src/components/Admin/Button/OpenOption.tsx
@@ -1,12 +1,15 @@
 import { OpenOptionDTO } from "types/Admin"
+import setting from "@assets/Header/setting.svg";
+import { Img } from "react-image";
 
 const OpenOption = ({calculateOptionsModalPosition} : OpenOptionDTO) => {
     return(
-        <div className={"w-[22px] h-[40px] right-[20px] top-[0px] absolute "}>
+        <div className={"w-[22px] h-[40px] right-[20px] top-[20px] absolute"}>
             <button
               className=" text-white text-3xl font-bold tracking-wider "
               onClick={(e) => calculateOptionsModalPosition(e)}
-            >...
+            >
+              <Img src={setting} alt='setting' />
             </button>
           </div>
     )
