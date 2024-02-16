fee6f327ad9454b64a92dd273449d81c666a434f
ys0222 <ggdbstjd0222@gmail.com>
2024-02-16 15:38:04 +0900

    Fix : 리프레시 토큰 만료시 리프레시 토큰 재발급 버그 수정

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -20,7 +20,6 @@ axiosInstance.interceptors.request.use(
     return Promise.reject(error);
   }
 );
-
 // axios 응답 후에 실행되는 interceptor
 axiosInstance.interceptors.response.use(
   (response) => {
@@ -29,31 +28,27 @@ axiosInstance.interceptors.response.use(
   async (error) => {
     const originalRequest = error.config;
 
-    // 응답이 401 Unauthorized인 경우 refreshToken으로 accessToken 재발급
     if (error.response.status === 401 && !originalRequest._retry) {
       originalRequest._retry = true;
 
-      try {
-        const refreshToken = localStorage.getItem('refreshToken');
-        const response = await axiosInstance.post('/login/token/reissue', {
-          refreshToken,
+      const refreshToken = localStorage.getItem('refreshToken');
+      axiosInstance.post('/login/token/reissue', { refreshToken })
+        .then(response => {
+          Cookies.set('accessToken', response.data.data.access_token);
+          console.log('토큰을 새로 발급 중입니다...');
+
+          originalRequest.headers.Authorization = `Bearer ${response.data.data.access_token}`;
+          return axiosInstance(originalRequest);
+        })
+        .catch(error => {
+          console.log('에러가 발생했습니다:', error);
+          
+            window.location.href = '/login';
+          
+          return Promise.reject(error);
         });
-        // 새로운 accessToken으로 localStorage 업데이트
-        Cookies.set('accessToken', response.data.data.access_token);
-        console.log('토큰을 새로 발급 중입니다...');
-
-        // 재발급된 accessToken으로 원래 요청 다시 보내기
-        originalRequest.headers.Authorization = `Bearer ${response.data.data.access_token}`;
-        return axiosInstance(originalRequest);
-      } catch (error) {
-        // refreshToken으로 재발급 실패 시 로그인 페이지로 이동 또는 오류 처리
-        console.log(error);
-        // 예: 로그인 페이지로 이동
-        window.location.href = '/login';
-        return Promise.reject(error);
-      }
+    } else {
+      return Promise.reject(error);
     }
-
-    return Promise.reject(error);
   }
 );
