7d70caf9c52bcf7d3577010d196e4ab71b5c6f92
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2024-02-18 20:20:28 +0900

    Feat - Remove unused code and fix carousel bug in Recommendation component

--- src/components/EditList/MusicList/MusicDataRow.tsx
+++ src/components/EditList/MusicList/MusicDataRow.tsx
@@ -4,7 +4,6 @@ import { MusicDataRowContent } from "./MusicContents";
 import { useSelector } from "react-redux";
 import { RootState } from "@store/index";
 import { MusicLength } from "./MusicLength";
-import InfiniteScroll from "react-infinite-scroller";
 import { musicListState } from "@atoms/Musics/MusicList";
 import { useRecoilValue } from "recoil";
 import SkeltonMusics from "@components/EditList/Skeleton/MusicSkeleton";
@@ -60,9 +59,6 @@ export const MusicDataRow = ({ isEditing, fetchPlaylist }: MusicDataDTO) => {
     },
     [selectedVideoIndex]
   );
-  const loadMore = useCallback(() => {
-    // TODO: Implement loadMore function
-  }, []);
 
   useEffect(() => {
     if (isEditing) {
