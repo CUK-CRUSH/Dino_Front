3d6524276a577c5498eda45c70099c0b34da8788
freecoderer <donggood7@naver.com>
2024-02-23 17:07:56 +0900

    Fix: Swiper 수정 했는데 잘 안됨

--- src/components/Recommend/Recommendation.tsx
+++ src/components/Recommend/Recommendation.tsx
@@ -5,6 +5,11 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 import Noimage from "@assets/noimage.jpg";
 import { useCookies } from "react-cookie";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
+import {EffectCoverflow, Mousewheel, Navigation} from 'swiper/modules';
 
 interface PlayListDTO {
   id: number;
@@ -36,41 +41,43 @@ const Recommendation = () => {
     window.location.reload();
   };
 
+
   return (
-    <div className="flex justify-center flex-col mx-5 ">
-      <p className="mb-2">✨추천 플레이리스트</p>
-      {data.length > 0 ? (
-        <Carousel
-          showArrows={true}
-          swipeable={true}
-          centerMode={true}
-          centerSlidePercentage={30}
-          showThumbs={false}
-          showStatus={false}
-          autoPlay={true}
-          transitionTime={1000}
-          infiniteLoop={false}
-          showIndicators={true}
-        >
-          {data.map((item) => (
-            <div
-              className="mr-3 cursor-pointer"
-              key={item.id}
-              onClick={() => handleOnClick(item.username, item.id)}
+      <div className="flex justify-center flex-col mx-5 ">
+        <p className="mb-2">✨추천 플레이리스트</p>
+        {data.length > 0 ? (
+            <Swiper
+                modules={[Pagination]}
+                grabCursor={true}
+                centeredSlides={true}
+                slidesPerView={4}
+                slidesOffsetBefore={0}
+                initialSlide={0} // Set the first slide as the initial slide
             >
-              <img
-                className="cursor-pointer w-[105px] h-[105px] rounded-lg object-cover"
-                src={item.thumbnailUrl ? item.thumbnailUrl : Noimage} // default.jpg는 기본 이미지 경로입니다.
-                alt={item.playlistName}
-              />
-              <p>{item.playlistName}</p>
-            </div>
-          ))}
-        </Carousel>
-      ) : (
-        <div>추천 플레이리스트가 없습니다.</div>
-      )}
-    </div>
+              {data.map((item) => (
+                  <SwiperSlide key={item.id}>
+                    <div
+                        className="mr-3 cursor-pointer w-[6rem] h-[10rem] flex flex-col items-center"
+                        onClick={() => handleOnClick(item.username, item.id)}
+                    >
+                      <div>
+                        <img
+                            className="w-[6rem] h-[6rem] cursor-pointer rounded-lg object-cover"
+                            src={item.thumbnailUrl ? item.thumbnailUrl : Noimage}
+                            alt={item.playlistName}
+                        />
+                        <div style={{wordWrap: 'break-word', wordBreak: 'break-all'}}>
+                          <h3 className="text-center mt-2 overflow-hidden text-overflow-ellipsis">{item.playlistName}</h3>
+                        </div>
+                      </div>
+                    </div>
+                  </SwiperSlide>
+              ))}
+            </Swiper>
+        ) : (
+            <div>추천 플레이리스트가 없습니다.</div>
+        )}
+      </div>
   );
 };
 
