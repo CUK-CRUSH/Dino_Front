629cac9dfc9623b5ccc6019dca9fea06878ccd15
ys0222 <ggdbstjd0222@gmail.com>
2024-02-23 16:33:03 +0900

    Feat : 검색 플레이리스트 스켈레톤 효과 구현

--- src/components/Search/SearchPlaylistDetail.tsx
+++ src/components/Search/SearchPlaylistDetail.tsx
@@ -6,6 +6,8 @@ import { getPlaylistDTO } from 'types/Admin';
 import { useInView } from 'react-intersection-observer';
 import OptionHeader from '@components/Layout/optionHeader';
 import QueryText from './QueryText';
+import SkeltonPlaylist from '@components/Admin/SkeltonPlaylist';
+import { useCustomMargin } from '@hooks/useCustomMargin/useCustomMargin';
 
 const SearchPlaylistDetail: React.FC = () => {
   const location = useLocation();
@@ -13,7 +15,10 @@ const SearchPlaylistDetail: React.FC = () => {
   const [count, setCount] = useState<number>(0);
   const [isLast, setLast] = useState<boolean>(false);
   const [page, setPage] = useState(0); // 현재 페이지를 저장할 상태
-
+  const [isLoading, setIsLoding] = useState<boolean>(true);
+  
+  // skelton margin
+  const customMargin = useCustomMargin();
   // URL 파라미터 읽기
   const searchParams = new URLSearchParams(location.search);
   const query = searchParams.get('query');
@@ -24,7 +29,7 @@ const SearchPlaylistDetail: React.FC = () => {
   // API 호출
   const fetchData = async () => {
     try {
-      const searchResult = await getSearchPlaylist(query, page);
+      const searchResult = await getSearchPlaylist(query, page,setIsLoding);
       setPlaylistdata([...playlistData, ...searchResult.data]); // 기존 데이터에 새로운 데이터를 추가
       setPage((page) => page + 1);
       setCount(playlistData.length);
@@ -52,15 +57,17 @@ const SearchPlaylistDetail: React.FC = () => {
     <div className="w-full h-full min-h-screen relative flex flex-col bg-white scrollbar-hide overflow-scroll font-PretendardMedium">
       <OptionHeader text="검색" />
 
-      <main className='h-full pb-[80px] '>
+      <main className='h-full'>
 
       {query && <QueryText query={query} />}
       <p className='p-4'> 플레이리스트 </p>
+      
+      {isLoading && <SkeltonPlaylist customMargin={customMargin} /> }
 
-        {playlistData &&
-          playlistData.map((playlist: getPlaylistDTO, index: number) => (
-            <PlayList key={playlist.id} playlist={playlist} fontColor='#000' visible={true} />
-          ))}
+      {playlistData &&
+        playlistData.map((playlist: getPlaylistDTO, index: number) => (
+          <PlayList key={playlist.id} playlist={playlist} fontColor='#000' visible={true} />
+      ))}
 
         <div ref={ref} />
       </main>
