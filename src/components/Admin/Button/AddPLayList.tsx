fe6f27c8e8f56f2c2e267eba0bd486ebcdb76107
ys0222 <ggdbstjd0222@gmail.com>
2024-02-24 13:27:43 +0900

    Refac : AddPlaylist customMargin hook 사용

--- src/components/Admin/Button/AddPLayList.tsx
+++ src/components/Admin/Button/AddPLayList.tsx
@@ -6,27 +6,10 @@ import { useCookies } from "react-cookie";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { setToast } from "@reducer/Toast/toast";
+import { useCustomMargin } from "@hooks/useCustomMargin/useCustomMargin";
 export const AddPlayList = () => {
-  const { windowSize, isMobile } = useWindowSizeCustom();
 
-  const [customMargin, setCustomMargin] = useState<number>(0);
-
-  useEffect(() => {
-    if (!isMobile) {
-      setCustomMargin((390 / 2 - 151) / 2);
-    } else {
-      if (windowSize.width > 400 && windowSize.width <= 429) {
-        setCustomMargin((windowSize.width / 2 - 151) / 2);
-      } else if (windowSize.width >= 430) {
-        setCustomMargin((390 / 2 - 151) / 2);
-      } else if (windowSize.width >= 390 && windowSize.width <= 400 ) {
-        setCustomMargin((390 / 2 - 151) / 2);
-      }
-      else if (windowSize.width < 390) {
-        setCustomMargin((windowSize.width / 2 - 151) / 2);
-      }
-    }
-  }, [windowSize.width, isMobile]);
+ const customMargin = useCustomMargin();
 
   const [cookie] = useCookies();
   let token = cookie.accessToken;
