7bfd2c7c930dfdcff3e5045b512ce99e3cf34615
freecoderer <donggood7@naver.com>
2024-02-26 13:35:53 +0900

    fix

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,4 +1,4 @@
-gut import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getAnalytics, isSupported } from "firebase/analytics";
