ea5b12265c97143860a474e459b83032d93f8d7f
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2024-02-26 15:51:38 +0900

    Feat - Like 무한스크롤 성공 ㅎ

--- src/components/Likes/LikeList.tsx
+++ src/components/Likes/LikeList.tsx
@@ -42,51 +42,70 @@ const LikeList = () => {
 
   const { playlistId } = useParams<{ playlistId: string }>();
 
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState<any[]>([]);
   const [view, inView] = useInView();
+
+  const [isLast, setLast] = useState<boolean>(false);
+  const [page, setPage] = useState<number>(0);
 
   const playlistName = useRecoilValue(playlistNameState);
 
-  useEffect(() => {
-    const fetchLikeList = async () => {
-      try {
-        const response = await getLikeList(Number(playlistId));
-        setUsers(response.data);
-      } catch (error) {
-        console.error(error);
+  const fetchLikeList = async () => {
+    try {
+      const response = await getLikeList(Number(playlistId), page);
+      setUsers((prevUsers) => [...prevUsers, ...response.data]);
+
+      setPage((page) => page + 1);
+
+      if (response.data.length < 15) {
+        setLast(true); // 마지막 페이지의 항목 수를 사용하여 isLast를 설정
+      } else {
+        setLast(false);
       }
-    };
-    fetchLikeList();
-  }, [playlistId]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    if (inView && !isLast) {
+      fetchLikeList();
+    }
+  }, [inView]);
 
   const handleBack = useCallback(() => {
     navigate(-1);
   }, [navigate]);
-  return (
-    <div className="h-full w-full scrollbar-hide overflow-scroll flex flex-col bg-white text-black font-medium leading-[18px]">
-      <header className="flex h-[5%] smartPhoneXs:h-[3.5%] smartPhone:h-[3.5%] tabletMini:h-[3%] tablet:h-[3%] items-center justify-between m-3 text-[19px] border-b-[1px] border-[#EFEFEF]">
-        <button
-          type="button"
-          onClick={handleBack}
-          className="text-white self-start mt-2"
-        >
-          <FaAngleLeft size={24} color="black" />
-        </button>
-        <p className="text-center mx-auto">{playlistName}</p>
-      </header>
 
-      {/* 이만큼 API가져와서 Mapping */}
-      {users.length > 0 ? (
-        users.map((user: any) => (
-          <>
-            <UserProfile key={user.id} user={user} />
-            <InfiniteDiv view={view} />
-          </>
-        ))
-      ) : (
-        <p>좋아요가 없습니다.</p>
-      )}
-    </div>
+  console.log(page);
+  return (
+    <>
+      <div className="h-full w-full scrollbar-hide overflow-scroll flex flex-col bg-white text-black font-medium leading-[18px]">
+        <header className="flex h-[5%] smartPhoneXs:h-[3.5%] smartPhone:h-[3.5%] tabletMini:h-[3%] tablet:h-[3%] items-center justify-between m-3 text-[19px] border-b-[1px] border-[#EFEFEF]">
+          <button
+            type="button"
+            onClick={handleBack}
+            className="text-white self-start mt-2"
+          >
+            <FaAngleLeft size={24} color="black" />
+          </button>
+          <p className="text-center mx-auto">{playlistName}</p>
+        </header>
+        {/* 이만큼 API가져와서 Mapping */}
+        {users.length > 0 ? (
+          users.map((user: any) => (
+            <>
+              <UserProfile key={user.id} user={user} />
+            </>
+          ))
+        ) : (
+          <p>좋아요가 없습니다.</p>
+        )}
+        <div>
+          <InfiniteDiv view={view} />
+        </div>
+      </div>
+    </>
   );
 };
 
