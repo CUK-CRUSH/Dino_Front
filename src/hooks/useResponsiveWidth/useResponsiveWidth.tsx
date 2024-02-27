5a7689691c1d8395d0adbd873746ce17cbdcf0dd
ys0222 <ggdbstjd0222@gmail.com>
2024-02-27 15:33:25 +0900

    Design : AdminEdit 모달 width 반응형 디자인

---
+++ src/hooks/useResponsiveWidth/useResponsiveWidth.tsx
@@ -0,0 +1,37 @@
+
+import useWindowSizeCustom from "@hooks/useCustomMargin/useWindowSizeCustom";
+import { useEffect, useState } from "react";
+
+function useCompareToken() {
+  const { windowSize } = useWindowSizeCustom();
+
+  // 사이즈 390 보다 크면 모달창 크기 고정
+  const [size, setSize] = useState<string>();
+
+  useEffect(() => {
+    console.log(windowSize)
+    if (windowSize.width === 390) {
+      setSize("w-[390px]")
+      return;
+    }
+
+    if (windowSize.width === 414) {
+      setSize("w-[414px]")
+      return;
+    }
+
+    if ((windowSize.width < 390) || ((windowSize.width < 429) && (windowSize.width > 391))) {
+      setSize("w-full")
+      return;
+    }
+
+    if (windowSize.width >= 430) {
+      setSize('w-[390px]')
+    }
+
+  }, [windowSize.width]);
+
+  return size;
+}
+
+export default useCompareToken;
