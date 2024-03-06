86fbedc242b38ca24debb05eb46f5ba0310cb4af
ys0222 <ggdbstjd0222@gmail.com>
2024-03-06 13:28:13 +0900

    Fix : 배경 / 프로필 사진 loader 수정

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,9 @@
+import Spinner from '@assets/Spinner/Spinner.svg';
+
+export const Loader = () => {
+  return (
+    <div className="bg-white flex justify-content-center align-items-center" style={{height: '100vh'}}>
+      <img src={Spinner} className='w-[1px]'/>
+    </div>
+  )
+}
