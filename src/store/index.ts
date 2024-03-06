86f156821c8e430833956d303f58145bd8616631
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2024-03-06 16:13:33 +0900

    Delete - getDefaultMiddleware

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,4 @@
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import counterReducer from "@reducer/counter";
