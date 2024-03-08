994bdc5c34c8cad39655b8460ed63c20a2fba377
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2024-03-08 13:28:14 +0900

    Design - home v1.2

--- src/components/Home/home.tsx
+++ src/components/Home/home.tsx
@@ -6,30 +6,22 @@ const HomeComponent = () => {
   const { t } = useTranslation("Home");
 
   return (
-    <div className="w-full h-full scrollbar-hide overflow-y-auto">
-      <header
-        className={
-          "flex mt-5 h-[calc(8% - 54px)] flex-row-reverse justify-between align-middle"
-        }
-      >
-        <div className={"grow"}></div>
-        <img className={"m-4 w-[110px] "} src={Mylist} alt="Mylist" />
+    <div className="h-screen scrollbar-hide overflow-y-auto">
+      <header className="flex mt-5 h-[calc(8vh - 54px)] flex-row-reverse justify-between items-center">
+        <div className="flex-grow"></div>
+        <img className="m-4 w-[110px]" src={Mylist} alt="Mylist" />
       </header>
-      <main
-        className={
-          "flex flex-col items-center justify-center gap-4 h-[calc(80% - 82px)] mt-[38px] mb-[44px]"
-        }
-      >
+      <main className="flex flex-col items-center justify-center gap-4 h-[calc(80vh - 82px)] mt-[38px] mb-[44px]">
         <img
-          className="h-[600px] smartPhoneXs:h-[420px] smartPhone12:h-[540px] smartPhone:h-[520px]"
+          className="h-[600px] sm:h-[420px] md:h-[540px] lg:h-[520px]"
           src={mainGraphic}
           alt="Main Graphic"
         />
       </main>
-      <footer className="h-[12%] w-full flex justify-center items-center">
+      <footer className="h-[12vh] w-full flex justify-center items-center">
         <Link className="w-buttonWidth" to={"/login"}>
-          <div className="flex flex-row  h-[58px] bg-neutral-200 rounded-[30px] justify-center items-center">
-            <div className="text-[15px] font-normal font-['Pretendard']">
+          <div className="flex flex-row h-[58px] bg-neutral-200 rounded-[30px] justify-center items-center">
+            <div className="text-[15px] font-normal">
               <b>{t("makemylist")}</b>
             </div>
           </div>
