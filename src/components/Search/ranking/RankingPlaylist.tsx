15775f064e3e107a8877ccf5ef1cfba090105414
ys0222 <ggdbstjd0222@gmail.com>
2024-03-10 16:53:55 +0900

    Feat : 인기 플리 상세 페이지 스켈레톤 효과 적용

--- src/components/Search/ranking/RankingPlaylist.tsx
+++ src/components/Search/ranking/RankingPlaylist.tsx
@@ -24,7 +24,7 @@ const RankingPlaylist: React.FC = () => {
   }, [dispatch]);
 
   const customMargin = useCustomPlaylistMargin();
-  console.log(customMargin)
+  
   return (
     <div className="w-full h-full min-h-screen relative flex flex-col bg-white scrollbar-hide overflow-scroll font-PretendardMedium">
       <OptionHeader />
@@ -34,7 +34,7 @@ const RankingPlaylist: React.FC = () => {
         <p className="flex justify-center "><img src={hot} alt='x' /> &nbsp; 인기 플레이리스트</p>
         <p style={{ marginLeft: customMargin }} className={`py-4 font-PretendardBold underline underline-offset-4	`}>랭킹</p>
 
-        {isLoading && <SkeltonPlaylist customMargin={customMargin} />}
+        {status === 'loading' && <SkeltonPlaylist customMargin={customMargin} />}
 
         {searchPlaylistRankingData &&
           searchPlaylistRankingData.map((playlist: any, index: number) => (
