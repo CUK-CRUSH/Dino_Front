70607ed055f038bc05d7f43d2b6bddcbe04d4e01
ys0222 <ggdbstjd0222@gmail.com>
2024-03-18 13:54:22 +0900

    Design : 404 페이지 역공학

--- src/pages/NotFound/NotFonud.tsx
+++ src/pages/NotFound/NotFonud.tsx
@@ -1,30 +1,40 @@
 import {useNavigate} from 'react-router';
+import four from '@assets/Forbidden/404.svg';
+import Character from "@assets/Forbidden/Character.svg";
+import Footer from '@components/Layout/footer';
 
 const NotFound = () => {
   const navigate = useNavigate();
   const handleClick = () => {
     navigate('/');
   }
+
+  const style = {
+    backgroundImage: `url(${Character})`,
+  };
+
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-white">
-      <div className="flex items-center justify-center p-4">
-        <h1 className="text-6xl font-bold text-gray-900">404</h1>
-        <span className="mx-4 text-[15px] text-gray-600">|</span>
-        <div className="text-xl text-gray-600">
+    <div className="flex flex-col h-screen bg-no-repeat bg-right-bottom bg-[#171717] bg-charactor" >
+      <div className="p-[3%]">
+        <img src={four} alt='x' className='mt-[30%]'  />
+        <div className="text-xl text-[#f9f9f9] mt-[7%]">
           <h2 className="font-bold">페이지를 찾을 수 없습니다.</h2>
-          <p className="mt-2">
-            요청하신 페이지가 <br /> 존재하지 않거나 <br /> 이동되었습니다.
+          <p className="mt-2 text-[#f9f9f9]">
+            요청하신 페이지가 <br /> 존재하지 않거나 이동되었습니다.
           </p>
         </div>
       </div>
 
-      <div className="mt-8 w-full px-4">
+      <div className="w-[50%] mx-auto mt-[25%]">
         <button 
           onClick={handleClick}
-          className="w-full bg-black p-4 rounded-[35px] h-[60px] text-white">
+          className="w-full bg-white rounded-[43px] h-[50px] text-black font-PretendardBold">
           MyList로 돌아가기
         </button>
       </div>
+      <div className="absolute -bottom-[0px] w-full bg-transparent">
+          <Footer bgColor="transparents" />
+        </div>
     </div>
   );
 };
