ca57f49e6e5cd7393ca3cdba552b8eca7dd6ef88
ys0222 <ggdbstjd0222@gmail.com>
2024-03-20 12:13:10 +0900

    Refac : Favorites 캐싱 성공

--- src/components/Favorites/Favorites.tsx
+++ src/components/Favorites/Favorites.tsx
@@ -4,62 +4,52 @@ import SkeltonPlaylist from "@components/Admin/SkeltonPlaylist";
 import InfiniteDiv from "@components/InfiniteDiv/InfiniteDiv";
 import OptionHeader from "@components/Layout/optionHeader";
 import { useCustomPlaylistMargin } from "@hooks/useCustomMargin/useCustomPlaylistMargin";
+import { fetchFavoriteList, favoriteListSlice } from "@reducer/Favorites/favorites";
+import { AppDispatch, RootState } from "@store/index";
 
 import { useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
 import { useInView } from "react-intersection-observer";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import { getPlaylistDTO } from "types/Admin";
 
 const FavoritesPage: React.FC = () => {
   const [view, inView] = useInView();
-  const [isLast, setLast] = useState<boolean>(false);
-  const [page, setPage] = useState<number>(0); // 현재 페이지를 저장할 상태
   const [cookies] = useCookies();
   const token = cookies.accessToken;
-  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   // 플레이리스트 데이터
-  const [playlistData, setPlaylistdata] = useState<getPlaylistDTO[]>([]);
+  const dispatch = useDispatch<AppDispatch>();
 
-  // API 호출
-  const fetchData = async () => {
-    try {
-      const playlistResult = await getFavoritesPlayList(token, page, setIsLoading);
-      setPlaylistdata([...playlistData, ...playlistResult.data]); // 기존 데이터에 새로운 데이터를 추가
+  const favoriteList = useSelector((state: RootState) => state.favorite.favoriteList);
+  const currentPage = useSelector(
+    (state: RootState) => state.favorite.currentPage
+  );
+  const isLast = useSelector((state: RootState) => state.favorite.isLast);
+  const status = useSelector((state: RootState) => state.favorite.status);
 
-      setPage((page) => page + 1);
+  useEffect(() => {
+    if (status === "idle" && inView && !isLast) {
+      dispatch(fetchFavoriteList({ token: token, page:0 }));
 
-      if (playlistResult.data.length < 8) {
-        setLast(true);
-      } else {
-        setLast(false);
-
-      }
-
-    } catch (error) {
-      console.error(error);
     }
-  };
+    const delay = 200;
+    const timeoutId = setTimeout(() => {
+    }, delay);
+    return () => clearTimeout(timeoutId);
+  }, [status, dispatch,  currentPage, inView, isLast]);
 
   // skelton margin
   const customMargin = useCustomPlaylistMargin();
-
-  /* eslint-disable react-hooks/exhaustive-deps */
-  useEffect(() => {
-    if (inView && !isLast) {
-
-      fetchData();
-    }
-  }, [inView]);
 
   return (
     <div className="h-full min-h-screen w-full scrollbar-hide overflow-scroll flex  flex-col bg-white text-black text-[15px] font-medium leading-[18px]">
       <OptionHeader text='좋아요한 목록' />
 
       <div className="inline">
-        {isLoading && <SkeltonPlaylist customMargin={customMargin} />}
-        {playlistData &&
-          playlistData.map((playlist: getPlaylistDTO, index: number) => (
+        {favoriteList &&
+          favoriteList.map((playlist: getPlaylistDTO, index: number) => (
             <PlayList key={playlist.id} playlist={playlist} fontColor='#000' visible={true} />
           ))}
       </div>
