9f1a61c11217a3a49260f59d02a9fba28751fdc3
ys0222 <ggdbstjd0222@gmail.com>
2024-04-11 16:10:45 +0900

    Design : 초기 프로필 계속하기 버튼 위치 수정

--- src/components/SetProfile/Next.tsx
+++ src/components/SetProfile/Next.tsx
@@ -1,4 +1,5 @@
 import { updateMember } from "@api/member-controller/memberController";
+import useWindowSizeCustom from "@hooks/useCustomMargin/useWindowSizeCustom";
 import { RootState } from "@store/index";
 import { checkData } from "@utils/checkData/checkData";
 import { useCookies } from "react-cookie";
@@ -7,7 +8,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { UpdateMemberParams } from "types/AdminEdit";
 import { NextDTO, checkDataItem } from "types/SetProfile/setProfile";
 
-const Next = ({ step,username}: NextDTO) => {
+const Next = ({ step, username }: NextDTO) => {
   const navigate = useNavigate();
 
   const [cookie] = useCookies();
@@ -15,48 +16,49 @@ const Next = ({ step,username}: NextDTO) => {
 
   const { profileImage, profileBackgroundImage, profileIntroduction } = useSelector(
     (state: RootState) => state.setProfile
-  );  
+  );
   const updateMemberData: UpdateMemberParams = {
     username: undefined,
     introduction: profileIntroduction,
     profileImage: profileImage,
     backgroundImage: profileBackgroundImage,
     cookies: token,
-    }
+  }
 
-  
+
   const handleMember = async (data: UpdateMemberParams) => {
-    if(step === 3) { 
+    if (step === 3) {
       const code = await updateMember(data);
-      
-      if(code.status === 200) {
+
+      if (code.status === 200) {
         setTimeout(async () => {
           navigate(`/user/${code.data.username}`)
-        }, 1000);  
-        
+        }, 1000);
+
       }
     }
-  
+
   };
-  
-  const checkDataItem : checkDataItem[] = [
+
+  const checkDataItem: checkDataItem[] = [
     { key: 1, value: profileImage },
     { key: 2, value: profileBackgroundImage },
     { key: 3, value: profileIntroduction },
   ];
 
+  const {isMobile} = useWindowSizeCustom();
+
   return (
     <Link to={`/SetProfile/${username}/${step + 1}`}>
-      {checkData(checkDataItem,step) ? 
-      <div onClick={()=>handleMember(updateMemberData)} className="absolute bottom-0 -left-0 p-4 w-full bg-[#000] text-white flex items-center justify-center overflow-hidden">
+      <div
+        onClick={() => handleMember(updateMemberData)}
+        className={`${!isMobile ? "absolute " : "fixed "} bottom-0 -left-0 
+                      p-4 w-full text-white flex items-center justify-center overflow-hidden 
+                      ${checkData(checkDataItem, step) ? "bg-[#000]" : "bg-[#b6b6b6]"
+          }`}
+      >
         계속하기
       </div>
-      :
-      <div onClick={()=>handleMember(updateMemberData)} className="absolute bottom-0 -left-0 p-4 w-full bg-[#b6b6b6] text-white flex items-center justify-center overflow-hidden">
-        계속하기
-      </div>
-      }
-      
     </Link>
   );
 };
